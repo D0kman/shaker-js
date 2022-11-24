7d149cc659d0faa19e935f6d146ff61d322fd24b
D0kman <118976900+D0kman@users.noreply.github.com>
2022-11-24 14:10:32 +0300

    Some changes. Added js

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log(123);
+
+console.log(123);
+console.log(123);
+console.log(123);
+console.log(123);
+console.log(123);
+console.log(123);
+console.log(123);
+console.log(123);
+console.log(123);
+console.log(123);
+console.log(123);
